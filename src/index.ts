5fd6cad051e2efaf607b60a4ab32e23de93cc86d
Damilare <opeyemisamuel222@gmail.com>
2021-03-16 13:23:45 +0000

    triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'sam', age: 20 });
 
 user.on('change', ()=>{
-
+console.log('change number one')
 })
 
-
-console.log(user)
+user.trigger('change')
