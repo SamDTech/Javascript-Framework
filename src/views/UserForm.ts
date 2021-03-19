c37b319c82748a8b767f3c7e8ee1a1e37eb5e67a
Damilare <opeyemisamuel222@gmail.com>
2021-03-19 17:42:53 +0000

    building the user form

---
+++ src/views/UserForm.ts
@@ -0,0 +1,14 @@
+export class UserForm {
+  parent: Element;
+
+  template(): string {
+    return `<div>
+    <h1> User Form     </h1>
+      <Form>
+      <input />
+      </Form>
+
+    
+    </div>`;
+  }
+}
