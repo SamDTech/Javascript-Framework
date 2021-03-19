06aa84ebb38654ec8df8f0953257fbf93b137975
Damilare <opeyemisamuel222@gmail.com>
2021-03-19 10:44:57 +0000

    class methods for collection

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,28 @@
-import { Model } from "./Model";
-import {Attributes} from './Attributes';
-import {Eventing} from './Eventing'
-import { ApiSync } from "./ApiSync";
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { Eventing } from './Eventing';
+import { ApiSync } from './ApiSync';
+import { Collection } from './Collection';
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-const rootUrl = 'http://localhost:3000/users'
-
+const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
-  static buildUser(attrs: UserProps): User{
+  static buildUser(attrs: UserProps): User {
     return new User(
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
-
-    )
+    );
   }
- 
+
+  static buildUserCollections(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) => {
+      return User.buildUser(json);
+    });
+  } 
 }
