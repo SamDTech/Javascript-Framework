28f809e25551773a61c634c3b4156d35ef64690f
Damilare <opeyemisamuel222@gmail.com>
2021-03-19 09:22:46 +0000

    final refactor of user class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,24 @@
 import { Model } from "./Model";
-
+import {Attributes} from './Attributes';
+import {Eventing} from './Eventing'
+import { ApiSync } from "./ApiSync";
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users'
+
 
 export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User{
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
 
+    )
+  }
  
 }
