8fb33b5f7d91b1ae49a3bc63e915b5f71b7d9c60
Damilare <opeyemisamuel222@gmail.com>
2021-03-20 16:59:45 +0000

    reading input text

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,8 +14,17 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    const name = input.value;
+
+    this.model.set({ name });
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -27,8 +36,9 @@ export class UserForm {
               <div>User name: ${this.model.get('name')}</div>
                <div>User age: ${this.model.get('age')}</div>              
                       <input />
-               <button>Click Me</button>
+               <button class='set-name'>Change Name</button>
                <button class='set-age'>Set Random Image</button>
+
             </div>`;
   }
 
@@ -47,7 +57,7 @@ export class UserForm {
   }
 
   render(): void {
-    this.parent.innerHTML = ''
+    this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
 
     templateElement.innerHTML = this.template();
