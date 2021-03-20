f1f4d88eacc929081b0c9951996a6bc2f9cbcbc6
Damilare <opeyemisamuel222@gmail.com>
2021-03-20 16:53:31 +0000

    re-rendering dom element

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2245,7 +2245,17 @@ function () {
     this.onSetAgeClick = function () {
       _this.model.setRandomAge();
     };
+
+    this.bindModel();
   }
+
+  UserForm.prototype.bindModel = function () {
+    var _this = this;
+
+    this.model.on('change', function () {
+      _this.render();
+    });
+  };
 
   UserForm.prototype.eventsMap = function () {
     return {
@@ -2278,6 +2288,7 @@ function () {
   UserForm.prototype.render = function () {
     var _a;
 
+    this.parent.innerHTML = '';
     var templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
