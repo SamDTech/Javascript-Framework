7f7da9ac58c77308e7371b9bcf964de61ae4a5e5
Damilare <opeyemisamuel222@gmail.com>
2021-03-20 14:56:19 +0000

    adding user model properties

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { Collection } from './models/Collection';
 import { User, UserProps } from './models/User';
 import { UserForm } from './views/UserForm';
 
+const user = User.buildUser({name: 'NAME', age: 20})
 
-const userForm = new UserForm(document.querySelector('#root'))
+const userForm = new UserForm(document.querySelector('#root'), user)
 
 
 userForm.render()
--- src/models/User.ts
+++ src/models/User.ts
@@ -14,7 +14,7 @@ const rootUrl = 'http://localhost:3000/users';
 export class User extends Model<UserProps> {
   static buildUser(attrs: UserProps): User {
     return new User(
-      new Attributes<UserProps>(attrs),
+      new Attributes<UserProps>(attrs), 
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     );
