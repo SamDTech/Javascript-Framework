beda60f5655c7aeb499633e77fe204285eb773a3
Damilare <opeyemisamuel222@gmail.com>
2021-03-21 08:21:45 +0000

    regions map

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,17 +1,22 @@
 import { Model } from '../models/Model';
 import { User } from '../models/User';
 
-
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
   abstract template(): string;
 
- eventsMap(): { [key: string]: () => void }{
-   return {}
- }
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -33,6 +38,18 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (const key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
   render(): void {
     this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
@@ -40,6 +57,8 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+
+    this.mapRegions(templateElement.content);
 
     this.parent?.append(templateElement.content);
   }
