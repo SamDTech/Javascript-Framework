ba58840f31d6f189ff54870f1b39f80d61e63c52
Damilare <opeyemisamuel222@gmail.com>
2021-03-21 09:33:31 +0000

    collection views

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import { Collection } from "./models/Collection";
-import { User, UserProps } from "./models/User";
-import { UserList } from "./views/UserList";
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const users = new Collection('http://localhost:3000/users', (json: UserProps)=>{
-  return  User.buildUser(json)
-})
-
-users.on('change', ()=>{
-  const root = document.getElementById('root')
-
-  if(root){
-    new UserList(root, users).render()
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
   }
-})
+);
 
-users.fetch()
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
